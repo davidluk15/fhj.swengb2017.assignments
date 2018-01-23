ffe1c38a8cbb8e870e040b08e1b04e60d6761af2
davidluk15 <lukas.david@edu.fh-joanneum.at>
2018-01-23 23:58:20 +0100

    Game Name working

---
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxGameScreenPlayerA.scala
@@ -0,0 +1,39 @@
+package at.fhj.swengb.apps.battleship.jfx
+
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.fxml.{FXML, Initializable}
+import javafx.scene.control._
+import javafx.scene.layout.GridPane
+
+class BattleShipFxGameScreenPlayerA extends Initializable{
+
+
+
+
+
+
+  @FXML private var player1txt: TextField = _
+  @FXML private var gameTitle: TextField = _
+
+
+
+  @FXML def backToMain(): Unit = {
+    BattleShipFxApp.display(BattleShipFxApp.loadMainMenu,BattleShipFxApp.loadMain)
+  }
+
+  @FXML def playerB(): Unit = BattleShipFxApp.display(BattleShipFxApp.load("/at/fhj/swengb/apps/battleship/jfx/gameScreenPlayerB.fxml"),BattleShipFxApp.main)
+
+
+
+
+
+  override def initialize(url: URL, rb: ResourceBundle): Unit = {
+    player1txt.setText(BattleShipFxApp.playerOne)
+    gameTitle.setText(BattleShipFxApp.battleName)
+  }
+
+
+
+
+}
---
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxGameScreenPlayerB.scala
@@ -0,0 +1,40 @@
+package at.fhj.swengb.apps.battleship.jfx
+
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.fxml.{FXML, Initializable}
+import javafx.scene.control._
+import javafx.scene.layout.GridPane
+
+class BattleShipFxGameScreenPlayerB extends Initializable{
+
+
+
+
+
+
+  @FXML private var player2txt: TextField = _
+  @FXML private var gameTitle: TextField = _
+
+
+
+  @FXML def backToMain(): Unit = {
+    BattleShipFxApp.display(BattleShipFxApp.loadMainMenu,BattleShipFxApp.loadMain)
+  }
+
+  @FXML def playerA(): Unit = BattleShipFxApp.display(BattleShipFxApp.load("/at/fhj/swengb/apps/battleship/jfx/gameScreenPlayerA.fxml"),BattleShipFxApp.main)
+
+
+
+
+
+
+  override def initialize(url: URL, rb: ResourceBundle): Unit = {
+    player2txt.setText(BattleShipFxApp.playerTwo)
+    gameTitle.setText(BattleShipFxApp.battleName)
+  }
+
+
+
+
+}
